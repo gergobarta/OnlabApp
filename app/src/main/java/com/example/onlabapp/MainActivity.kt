8056d6072a66461b7aa97bb4a01df3008f730e0f
Gergo <bgergo113@gmail.com>
2022-03-13 00:33:09 +0100

    DrawerMenu

--- app/src/main/java/com/example/onlabapp/MainActivity.kt
+++ app/src/main/java/com/example/onlabapp/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.onlabapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.onlabapp.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val drawerLayout: DrawerLayout=binding.drawerLayout
+        val navView: NavigationView=binding.navView
+
+        toggle= ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(toggle.onOptionsItemSelected(item)){
+            true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
---
+++ app/src/main/java/com/example/onlabapp/adapter/ProductsAdapter.kt
@@ -0,0 +1,5 @@
+package com.example.onlabapp.adapter
+
+class ProductsAdapter {
+
+}
